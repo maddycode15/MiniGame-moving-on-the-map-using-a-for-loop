046bf27ea1418e1003b3838d0b7a9a13a8d5dfd1
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-10-31 15:40:32 -0300

    Initial commit

---
+++ MiniGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("MiniGame");
+            Console.WriteLine();
+
+            // count 1 to 10;
+            for (int i = 1; i <= 10; i = i + 1)  // i++ is an increment shortcut
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey(true);
+
+        }
+    }
+}
