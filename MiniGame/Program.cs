25cdd0bb2059a109e725c4d21c8a3756faad6599
madeline allison restin <144450180+maddycode15@users.noreply.github.com>
2023-10-31 16:06:12 -0300

    update

--- MiniGame/Program.cs
+++ MiniGame/Program.cs
@@ -14,11 +14,18 @@ namespace MiniGame
             Console.WriteLine("MiniGame");
             Console.WriteLine();
 
-            // count 1 to 10;
+            // count 1 to 10 using a for loop
             for (int i = 1; i <= 10; i = i + 1)  // i++ is an increment shortcut
             {
                 Console.WriteLine(i);
             }
+
+
+            // a value declaired inside a for loop will only exist within the context
+            // of that loop, you must declair and use it outside the loop in order for 
+            // the loop to be referanced outside of the loop
+
+            //for example (int i = 1; i <= 10; i = i + 1)
 
             Console.WriteLine();
             Console.WriteLine("press any key to exit");
